3f4640efd7e121e6a94a76e0864365663814b2b8
Ingrid <git@ingrids.email>
2021-12-12 23:47:19 +0100

    day 2 lua

---
+++ 02/2.lua
@@ -0,0 +1,42 @@
+local file = "input"
+
+local hor = 0
+local dep = 0
+
+for line in io.lines(file) do
+    local match = string.gmatch(line, "%S+")
+    local dir = match()
+    local amp = match()
+
+    -- print("dir:", dir, "amp:", amp)
+    if dir == "forward" then
+        hor = hor + amp
+    elseif dir == "down" then
+        dep = dep + amp
+    else
+        dep = dep - amp
+    end
+end
+
+print("part1:", hor * dep)
+
+hor = 0
+dep = 0
+local aim = 0
+
+for line in io.lines(file) do
+    local match = string.gmatch(line, "%S+")
+    local dir = match()
+    local amp = match()
+
+    if dir == "forward" then
+        hor = hor + amp
+        dep = dep + (amp * aim)
+    elseif dir == "down" then
+        aim = aim + amp
+    else
+        aim = aim - amp
+    end
+end
+
+print("part2:", hor * dep)
