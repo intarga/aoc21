43c9e9f724f599af8ab9c423d62709a7bff0ce08
Ingrid <git@ingrids.email>
2021-12-13 00:21:40 +0100

    day 1 haskell

---
+++ 01/1.hs
@@ -0,0 +1,18 @@
+-- stack --resolver lts script
+readInts :: String -> [Int]
+readInts = map read . lines
+
+pairDepths :: [Int] -> [(Int, Int)]
+pairDepths depths = zip (init depths) $ tail depths
+
+countIncreases = length . filter (\ (a,b) -> a < b)
+
+pairDepths3 :: [Int] -> [(Int, Int)]
+pairDepths3 depths = zip depths $ drop 3 depths
+
+main = do
+    contents <- readFile "input"
+    let part1 = show . countIncreases . pairDepths . readInts $ contents
+    let part2 = show . countIncreases . pairDepths3 . readInts $ contents
+    print $ "part1: " ++ part1
+    print $ "part2: " ++ part2
