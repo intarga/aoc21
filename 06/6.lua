f71f36af4c98e6870c1a6dbd823f3767409c0adb
Ingrid <intarga@protonmail.com>
2021-12-27 00:24:07 +0100

    day 6 lua

---
+++ 06/6.lua
@@ -0,0 +1,56 @@
+file = "input"
+
+fish = {}
+
+for i = 1,9,1 do
+    table.insert(fish, 0)
+end
+
+io.input(file)
+input = io.read("*all")
+
+for timer in input:gmatch("[0-9]+") do
+    timer = tonumber(timer) + 1
+    fish[timer] = fish[timer] + 1
+end
+
+function sim_day()
+    local new_fish = {}
+    for i = 1,9,1 do
+        table.insert(new_fish, 0)
+    end
+
+    for i = 1,#fish,1 do
+        new_fish[9] = fish[1]
+
+        for j = 8,1,-1 do
+            new_fish[j] = fish[j+1]
+        end
+
+        new_fish[7] = new_fish[7] + fish[1]
+    end
+
+    fish = new_fish
+end
+
+function count_fish()
+    local count = 0
+
+    for i = 1,#fish,1 do
+        count = count + fish[i]
+    end
+
+    return count
+end
+
+for i = 1,80,1 do
+    sim_day()
+end
+
+print("part1:", count_fish(fish))
+
+for i = 1,(256-80),1 do
+    sim_day()
+end
+
+print("part2:", count_fish(fish))
---
+++ 06/6naive.lua
@@ -0,0 +1,33 @@
+file = "input"
+
+fish = {}
+
+io.input(file)
+input = io.read("*all")
+
+for timer in input:gmatch("[0-9]+") do
+    table.insert(fish, tonumber(timer))
+end
+
+function sim_day()
+    for i = 1,#fish,1 do
+        if fish[i] > 0 then
+            fish[i] = fish[i] - 1
+        else
+            table.insert(fish, 8)
+            fish[i] = 6
+        end
+    end
+end
+
+for i = 1,80,1 do
+    sim_day()
+end
+
+print("part1:", #fish)
+
+for i = 1,(256-80),1 do
+    sim_day()
+end
+
+print("part2:", #fish)
